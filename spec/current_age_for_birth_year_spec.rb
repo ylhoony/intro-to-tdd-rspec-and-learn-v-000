cda9c681ad0c7a04c53d794822ba7efb6aeb9601
Hoon Lee <yl.hoony@gmail.com>
2017-10-24 03:14:44 +0000

    automatically backed up by learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -3,7 +3,7 @@ require_relative '../current_age_for_birth_year.rb'
 # describe "current_age_for_birth_year method" do
 #   it "returns the age of a person based on the year of birth" do
 #     age_of_person = current_age_for_birth_year(1984)
-# 
+#
 #     expect(age_of_person).to eq(19)
 #   end
 # end
@@ -13,7 +13,7 @@ describe "current_age_for_birth_year method" do
     current_year = Time.now.year
     birth_year = 1984
     answer = current_year - birth_year
- 
+
     age_of_person = current_age_for_birth_year(birth_year)
     expect(age_of_person).to eq(answer)
   end
